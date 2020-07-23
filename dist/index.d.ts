5fa7eae80dc391087eebdc32379acbf8bfab39fc
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-07-23 22:28:50 +0800

    dist

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export declare function vh(percent: number): number;
+export declare function vw(percent: number): number;
+export declare function vmax(percent: number): number;
+export declare function vmin(percent: number): number;
+//# sourceMappingURL=index.d.ts.map
